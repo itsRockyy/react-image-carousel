2b69ecd147d88f43353269abf187d6cfc1fa7f08
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-06-13 00:00:00 +0530

    feat(app): Basic Carousel functionalty

--- src/App.js
+++ src/App.js
@@ -2,30 +2,39 @@ import React from "react";
 import "./App.css";
 
 class Carousel extends React.Component {
+  state = {
+    activeImage: null,
+    totalImages: 0,
+  };
+
+  addImage = (event) => {
+    const {
+      target: { files = null },
+    } = event;
+
+    if (!files.length) return;
+
+    const reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+
+    reader.onload = (e) => {
+      document.querySelector(
+        ".carousel"
+      ).innerHTML += `<img id="${this.state.totalImages}-img" src=${e.target.result} class="carousel-image"></img>`;
+      this.setState({
+        totalImages: this.state.totalImages + 1,
+      });
+    };
+  };
+
   render() {
     return (
       <div className="App">
         <div className="image-upload-area">
-          <input type="file" name="" id="" />
+          <input type="file" name="file" onChange={this.addImage} />
           <div className="loaded-images"></div>
         </div>
-        <div className="carousel">
-          <img
-            src="https://i.picsum.photos/id/848/200/200.jpg?hmac=9pGbbeC1Q-zsi7TeMrGb93-TjKBmqPVY-tYuubIIqyw"
-            alt=""
-            id="i2"
-          />
-          <img
-            src="https://i.picsum.photos/id/492/200/200.jpg?hmac=XU2aUuiOp-6a1CXwWFsbEobU3xA-9upNSkJiJacygTM"
-            alt=""
-            id="i3"
-          />
-          <img
-            src="https://i.picsum.photos/id/326/200/200.jpg?hmac=T_9V3kc7xrK46bj8WndwDhPuvpbjnAM3wfL_I7Gu6yA"
-            alt=""
-            id="i4"
-          />
-        </div>
+        <div className="carousel"></div>
       </div>
     );
   }
